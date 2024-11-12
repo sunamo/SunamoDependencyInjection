c249792f8cab54362b08f1d369ef62c0f1376e32
sunamo.cz <radek.jancik@sunamo.cz>
2024-11-12 09:19:10 +0100

    24.11.12.1 First code

--- DummyServiceProvider.cs
+++ DummyServiceProvider.cs
@@ -1,5 +1,8 @@
 ﻿namespace SunamoDependencyInjection;
 
+/// <summary>
+/// Toto mi moc nepomůže. Sice bych neměl chybu že může být ServiceProvider potenciálně null ale zase nemůžu získat servisu přes GetService<T>(). Pouze přes GetService(Type). O dalších metodách ani nemluvě. 
+/// </summary>
 public class DummyServiceProvider : IServiceProvider
 {
     public object GetService(Type serviceType)
