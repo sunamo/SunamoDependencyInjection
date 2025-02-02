dfab70b2e78af68a1214c41766d165abe03a245a
sunamo.cz <radek.jancik@sunamo.cz>
2025-02-02 14:43:51 +0100

    25.2.2.3 Make class public

--- StaticLogger.cs
+++ StaticLogger.cs
@@ -2,13 +2,8 @@ namespace SunamoDependencyInjection;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-internal class StaticLogger
+public class StaticLogger
 {
     /// <summary>
     /// It has to be a variable, not a property. So that it doesn't look weird. Better one warning here that I can suppress than a million occurrences that look strangely.
