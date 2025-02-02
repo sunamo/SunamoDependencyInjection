58b26a825b15b44c9c816473c25cf71bb1bb731c
sunamo.cz <radek.jancik@sunamo.cz>
2025-02-02 14:38:02 +0100

    25.2.2.2 Make field from property

--- StaticLogger.cs
+++ StaticLogger.cs
@@ -10,5 +10,8 @@ using System.Threading.Tasks;
 
 internal class StaticLogger
 {
-    protected static ILogger Logger { get; set; } = NullLogger.Instance;
+    /// <summary>
+    /// It has to be a variable, not a property. So that it doesn't look weird. Better one warning here that I can suppress than a million occurrences that look strangely.
+    /// </summary>
+    protected static ILogger logger = NullLogger.Instance;
 }
